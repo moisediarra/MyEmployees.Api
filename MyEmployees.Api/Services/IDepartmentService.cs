2dc4b12b41748af6071e0967f51cfb2ef07ccdea
Moise DIARRA <moisepr@gmail.com>
2026-01-31 19:04:47 +0000

    Update IDepartmentService

---
+++ MyEmployees.Api/Services/IDepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using MyEmployees.Api.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyEmployees.Api.Services
+{
+    public interface IDepartmentService
+    {
+        Task<IEnumerable<DepartmentDto>> GetAllDepartmentsAsync();
+        Task<DepartmentDto?> GetDepartmentByIdAsync(int id);
+        Task<DepartmentDto> AddDepartmentAsync(CreateDepartmentDto departmentDto);
+        Task UpdateDepartmentAsync(int id, CreateDepartmentDto departmentDto);
+        Task DeleteDepartmentAsync(int id);
+    }
+}
