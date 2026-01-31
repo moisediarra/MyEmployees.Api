e6a433e49a194f3381a6c67f4cc2ed0216574cad
Moise DIARRA <moisepr@gmail.com>
2026-01-31 19:06:10 +0000

    Update of Program.cs

--- MyEmployees.Api/Program.cs
+++ MyEmployees.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using MyEmployees.Api.Data;
+using MyEmployees.Api.Mapping;
+using MyEmployees.Api.Repositories;
+using MyEmployees.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +13,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 // Configure DbContext with SQL Server
 builder.Services.AddDbContext<MyEmployeesDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
+// Add Repositories
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+// Add Services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
