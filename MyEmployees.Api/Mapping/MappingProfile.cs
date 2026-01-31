a10c9a67c976950ac71a6dedddc46bd3f3c17f21
Moise DIARRA <moisepr@gmail.com>
2026-01-31 16:22:30 +0000

    Add MappingProfile

--- MyEmployees.Api/Mapping/MappingProfile.cs
+++ MyEmployees.Api/Mapping/MappingProfile.cs
@@ -1,6 +1,19 @@
-﻿namespace MyEmployees.Api.Mapping
+﻿using AutoMapper;
+using MyEmployees.Api.DTOs;
+using MyEmployees.Api.Models;
+
+namespace MyEmployees.Api.Mapping
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>()
+                .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.Name));
+            CreateMap<CreateEmployeeDto, Employee>();
+
+            CreateMap<Department, DepartmentDto>();
+            CreateMap<CreateDepartmentDto, Department>();
+        }
     }
 }
