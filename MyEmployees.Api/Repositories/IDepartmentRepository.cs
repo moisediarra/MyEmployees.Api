690f8dbb4d2f2110c5a9b4a1843d1c509196edda
Moise DIARRA <moisepr@gmail.com>
2026-01-31 16:35:42 +0000

    Add IDepartmentRepository

---
+++ MyEmployees.Api/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyEmployees.Api.Models;
+namespace MyEmployees.Api.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetAllDepartmentsAsync();
+        Task<Department?> GetDepartmentByIdAsync(int id);
+        Task AddDepartmentAsync(Department department);
+        Task UpdateDepartmentAsync(Department department);
+    }
+}
